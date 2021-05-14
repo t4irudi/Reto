e00c7e04c2c17d4d69e23ba4cb3be3b360f231cf
Dam1-Jonpu <Dam1-Jonpu@DESKTOP-FLCMLJV>
2021-05-14 09:10:40 +0200

    vb insertado

---
+++ Proyecto/T4 Irudi Visual/T4 Irudi Visual/ModuleSql.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+
+Module ModuleSql
+
+    Public Function conectar() As MySqlConnection
+        Dim conn As New MySqlConnection
+        Dim myConnectionString As String
+
+        myConnectionString = "server=192.168.6.169;" _  '53 root "" prueba, tabla pruebas
+            & "uid=almi;" _
+            & "pwd=Almi123;" _
+            & "database=t4irudi"
+
+        Try
+            conn.ConnectionString = myConnectionString
+            conn.Open()
+            Return conn
+            'Dim command As New MySqlCommand("SELECT * FROM users", conn)
+            'Dim reader As MySqlDataReader = command.ExecuteReader()
+            'Dim dt As New DataTable()
+            'dt.Load(reader)
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+            Return Nothing
+        End Try
+
+    End Function
+
+
+End Module
---
+++ Proyecto/T4 Irudi Visual/T4 Irudi Visual/Reglas.vb
@@ -0,0 +1,9 @@
+﻿Public Class Reglas
+
+    Public frmPrincipal As Principal
+
+    Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
+        frmPrincipal.Show()
+        Me.Close()
+    End Sub
+End Class
